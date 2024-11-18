5e95493cd5504fff324dfc6a3d2d5f3ac1cb386c
Janier <janierf.quinonesq@konradlorenz.edu.co>
2024-11-18 18:06:09 -0500

    Cambios en navbar para el menu superior estilo riot games

--- GestionBibliotecaX/src/estilosComponentes/NavBar.jsx
+++ GestionBibliotecaX/src/estilosComponentes/NavBar.jsx
@@ -1,15 +1,15 @@
 // src/components/NavBar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css';
+import './NavBar.css'; // Asegúrate de que el CSS esté importado
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="nav-link">Inicio</Link>
-      <Link to="/colecciones" className="nav-link">Colecciones</Link>
-      <Link to="/biblioteca" className="nav-link">Biblioteca</Link>
-      <Link to="/acerca" className="nav-link">Acerca de</Link>
+      <Link to="/" className="nav-item">Inicio</Link>
+      <Link to="/colecciones" className="nav-item">Colecciones</Link>
+      <Link to="/biblioteca" className="nav-item">Biblioteca</Link>
+      <Link to="/acerca" className="nav-item">Acerca de</Link>
     </nav>
   );
 };
